ba4ae5c2f64a2ecbf602abe2fc36366e385eb4b4
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-04-06 13:00:30 -0700

    HackwichTen added segmentedControlPressed

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //sets up the initial conditions for the app upon launch
+        
+        myLabel.text = "Index One selected on the Segmented Control"
+        
     }
-
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+    }
+    
 
 }
 
